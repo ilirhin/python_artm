904cd484495651ce697d3579a015afeb407750c6
tylorn <tylorn@yandex-team.ru>
2019-01-09 05:39:54 +0300

    fix casts

--- pyartm/calculations/inner_product/cy_impl.pyx
+++ pyartm/calculations/inner_product/cy_impl.pyx
@@ -7,20 +7,22 @@ cimport cython
 @cython.boundscheck(False)
 @cython.wraparound(False)
 def memory_efficient_inner1d(
-        np.ndarray[np.float64_t, ndim=2] fst_arr,
-        np.ndarray[np.int32_t] fst_indices,
-        np.ndarray[np.float64_t, ndim=2] snd_arr,
-        np.ndarray[np.int32_t] snd_indices
+        double[:, :] fst_arr,
+        long[:] fst_indices,
+        double[:, :] snd_arr,
+        long[:] snd_indices
 ):
     cdef int size = fst_indices.shape[0]
     cdef int width = fst_arr.shape[1]
     cdef np.ndarray[np.float64_t] result = np.zeros(size, dtype=np.float64)
     cdef int i, j, fst_index, snd_index
+    cdef double sum
     for i in range(size):
-        result[i] = 0
+        sum = 0
         fst_index = fst_indices[i]
         snd_index = snd_indices[i]
         for j in range(width):
-            result[i] += fst_arr[fst_index, j] * snd_arr[snd_index, j]
+            sum += fst_arr[fst_index, j] * snd_arr[snd_index, j]
+        result[i] = sum
 
     return result
