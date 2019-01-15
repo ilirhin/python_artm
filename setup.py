ae5d3e99cdccfc7ecf464a835c9c96b840628e38
Ilya Irhin <tylorn@yandex-team.ru>
2019-01-16 00:47:37 +0300

    setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,19 @@
 #!/usr/bin/env python
 from __future__ import print_function
 
-import platform
-from distutils.core import setup
-from distutils.extension import Extension
-from setuptools import find_packages
-
 try:
     from Cython.Build import build_ext
     import numpy as np
-
-    extra_setup_args = dict(
-        ext_modules = [Extension(
-            "pyartm.calculations.inner_product.cy_impl",
-            sources=["pyartm/calculations/inner_product/cy_impl.pyx"],
-            include_dirs=[np.get_include()],
-        )],
-        cmdclass={'build_ext': build_ext}
-    )
+    np_include_dirs = np.get_include()
 except ImportError:
-    extra_setup_args = dict()
+    from setuptools.command.build_ext import build_ext
+    np_include_dirs = ''
 
+import platform
+
+from distutils.core import setup
+from distutils.extension import Extension
+from setuptools import find_packages
 
 import pyartm
 
@@ -44,8 +37,13 @@ setup(
                 ' of ARTM algorithm',
     long_description='',
     setup_requires=requirements[:2],
+    ext_modules=[Extension(
+        "pyartm.calculations.inner_product.cy_impl",
+        sources=["pyartm/calculations/inner_product/cy_impl.pyx"],
+        include_dirs=[np_include_dirs],
+    )],
+    cmdclass={'build_ext': build_ext},
     zip_safe=False,
     install_requires=requirements,
-    packages=find_packages(include=included_packages),
-    **extra_setup_args
+    packages=find_packages(include=included_packages)
 )
